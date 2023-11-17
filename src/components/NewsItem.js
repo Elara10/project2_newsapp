b2c0c0e456f9d408aa9a71e2572aba9139bda0c7
Ishakumari10 <isha02740@gmail.com>
2023-11-17 23:17:12 +0530

    Lec-24 completed

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react'
 
-export default class newsitem extends Component {
+
+export  class NewsItem extends Component {
   render() {
+   let {title , description}=this.props;
     return (
       <div>
-        This is a news item2.
+         <div  className="card" style={{width: "18rem"}}>
+           <img src="https://ichef.bbci.co.uk/live-experience/cps/624/cpsprodpb/10B48/production/_131742486_gettyimages-483386686-1.jpg"  className="card-img-top" alt="..."/>
+             <div  className="card-body">
+           <h5  className="card-title">{title}</h5>
+           <p  className="card-text">{description}</p>
+            <a href="/newsdetail"  className="btn btn-primary">Go somewhere</a>
       </div>
+     </div>    
+    </div>
     )
   }
 }
+
+export default NewsItem
