dcaa52f732987a973f6f55cebbef82c91cc91f4b
Ishakumari10 <isha02740@gmail.com>
2023-11-18 23:12:35 +0530

    Lec-25 completed

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,16 +2,18 @@ import React, { Component } from 'react'
 
 
 export  class NewsItem extends Component {
+ 
   render() {
-   let {title , description}=this.props;
+   let {title , description,imageUrl,newsUrl}=this.props;
     return (
       <div>
+        <div className="my-3"></div>
          <div  className="card" style={{width: "18rem"}}>
-           <img src="https://ichef.bbci.co.uk/live-experience/cps/624/cpsprodpb/10B48/production/_131742486_gettyimages-483386686-1.jpg"  className="card-img-top" alt="..."/>
+           <img src={imageUrl}  className="card-img-top" alt="..."/>
              <div  className="card-body">
            <h5  className="card-title">{title}</h5>
            <p  className="card-text">{description}</p>
-            <a href="/newsdetail"  className="btn btn-primary">Go somewhere</a>
+            <a href="/newsdetail"  className="btn btn-sm btn-primary">Read</a>
       </div>
      </div>    
     </div>
